1a18b2148b115a7890b21d6f6d0cf014eea630cb
Đỗ Quang Duy <105569897+quangduyw@users.noreply.github.com>
2023-02-09 22:55:14 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+curl -s https://raw.githubusercontent.com/quangduyw/runtool/main/main -o $home/usr/bin/toaidz
+chmod 777 $home/usr/bin/dhtool3.7
+
+printf '\n\033[1;32m Gõ \033[1;33mdhtool3.7 \033[1;32mĐể Vào Tool <3\n\n'
